6c6ee17a3b881a9f10ea1e13f18aeb7260db9905
AndreyAndreyQ11 <aayy6491@gmail.com>
2023-02-05 15:51:54 +0200

    test2

---
+++ src/component/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './App.css';
+
+import Form from "./Form/Form"
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <Form />
+    );
+  };
+};
---
+++ src/component/Form/Form.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+// import s from "./StartFild.module.css"
+
+
+
+export default class Form extends Component {
+
+
+    render() {
+
+        return (
+            <form >
+                <label>
+                    имя <input type="text"></input>
+                </label>
+
+            </form>
+        )
+
+    }
+}
